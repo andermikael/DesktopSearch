81c461f5a3333e3b322f5d1d97c57e91e8cf874d
andersmikael <anders@andis59.se>
2022-03-05 11:36:51 +0100

    Moved indexing to a new dialog. All indexing will be done in IndexForms Add posibility to add to the index

---
+++ DesktopSearch1/IndexForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DesktopSearch1
+{
+    partial class IndexForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonIndex = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.AddToIndexButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.AddToIndexButton);
+            this.groupBox1.Controls.Add(this.buttonClean);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonBrowse);
+            this.groupBox1.Controls.Add(this.textBoxPath);
+            this.groupBox1.Controls.Add(this.buttonIndex);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 112);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Index";
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonClean.Location = new System.Drawing.Point(304, 83);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(75, 23);
+            this.buttonClean.TabIndex = 5;
+            this.buttonClean.Text = "Clean up";
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Index and search the following folder:";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonBrowse.Location = new System.Drawing.Point(670, 48);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Location = new System.Drawing.Point(16, 48);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(638, 20);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // buttonIndex
+            // 
+            this.buttonIndex.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonIndex.Location = new System.Drawing.Point(184, 83);
+            this.buttonIndex.Name = "buttonIndex";
+            this.buttonIndex.Size = new System.Drawing.Size(112, 23);
+            this.buttonIndex.TabIndex = 3;
+            this.buttonIndex.Text = "Rebuild the index";
+            this.buttonIndex.Click += new System.EventHandler(this.buttonIndex_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 147);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(758, 91);
+            this.labelStatus.TabIndex = 9;
+            // 
+            // AddToIndexButton
+            // 
+            this.AddToIndexButton.Location = new System.Drawing.Point(19, 83);
+            this.AddToIndexButton.Name = "AddToIndexButton";
+            this.AddToIndexButton.Size = new System.Drawing.Size(75, 23);
+            this.AddToIndexButton.TabIndex = 6;
+            this.AddToIndexButton.Text = "Add";
+            this.AddToIndexButton.UseVisualStyleBackColor = true;
+            this.AddToIndexButton.Click += new System.EventHandler(this.AddToIndexButton_Click);
+            // 
+            // IndexForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 247);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelStatus);
+            this.Name = "IndexForm";
+            this.Text = "IndexForm";
+            this.Load += new System.EventHandler(this.IndexForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonIndex;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button AddToIndexButton;
+    }
+}
---
+++ DesktopSearch1/IndexForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Windows.Forms;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using DesktopSearch1.Icons;
+using DesktopSearch1.Parsing;
+using ZetaLongPaths;
+using System.IO;
+using System.Drawing;
+
+namespace DesktopSearch1
+{
+    public partial class IndexForm : Form
+    {
+        private string pathIndex;
+        private IndexWriter indexWriter;
+        private string[] patterns = { "*.doc", "*.xls", "*.ppt", "*.htm", "*.txt" };
+        private IndexSearcher searcher = null;
+
+        // statistics
+        private long bytesTotal = 0;
+
+        private int countTotal = 0;
+        private int countSkipped = 0;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public IndexForm()
+        {
+            InitializeComponent();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+        }
+
+        /// <summary>
+        /// Event handler for click ontbuttonIndex
+        /// </summary>
+        /// <param name="sender">Not Used</param>
+        /// <param name="e">Not Used</param>
+        private void buttonIndex_Click(object sender, System.EventArgs e)
+        {
+            BuildIndex(true);
+        }
+
+        /// <summary>
+        /// Build index
+        /// </summary>
+        /// <param name="create">true if we should create a new index, false if we should add to the current</param>
+        private void BuildIndex(bool create)
+        {
+            indexWriter = new IndexWriter(this.pathIndex, new StandardAnalyzer(), create);
+
+            bytesTotal = 0;
+            countTotal = 0;
+            countSkipped = 0;
+
+            enableControls(false);
+
+            ZlpDirectoryInfo di = new ZlpDirectoryInfo(this.textBoxPath.Text);
+
+            DateTime start = DateTime.Now;
+
+            addFolder(di);
+
+            string summary = String.Format("Done. Indexed {0} files ({1} bytes). Skipped {2} files.", countTotal, bytesTotal, countSkipped);
+            summary += String.Format(" Took {0}", (DateTime.Now - start));
+            status(summary);
+            enableControls(true);
+
+            indexWriter.Optimize();
+            indexWriter.Close();
+        }
+
+        /// <summary>
+        /// Turns the controls on or off.
+        /// </summary>
+        /// <param name="enable"></param>
+        private void enableControls(bool enable)
+        {
+            this.textBoxPath.Enabled = enable;
+            this.buttonIndex.Enabled = enable;
+            this.buttonBrowse.Enabled = enable;
+            this.buttonClean.Enabled = enable;
+        }
+
+        private void IndexForm_Load(object sender, EventArgs e)
+        {
+            this.textBoxPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            this.pathIndex = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DesktopSearch");
+            checkIndex();
+        }
+
+        /// <summary>
+        /// Updates the status label.
+        /// </summary>
+        /// <param name="msg"></param>
+        private void status(string msg)
+        {
+            status(msg, false);
+        }
+
+        private void status(string msg, bool error)
+        {
+            this.labelStatus.Text = msg;
+
+            if (error)
+                this.labelStatus.ForeColor = Color.Red;
+            else
+                this.labelStatus.ForeColor = DefaultForeColor;
+
+            Application.DoEvents();
+        }
+
+        private void checkIndex()
+        {
+            try
+            {
+                searcher = new IndexSearcher(this.pathIndex);
+                searcher.Close();
+            }
+            catch (IOException)
+            {
+                status("The index doesn't exist or is damaged. Please rebuild it.", true);
+                return;
+            }
+
+            string msg = String.Format("Index is ready. It contains {0} documents.", searcher.MaxDoc());
+            status(msg);
+        }
+
+        // <summary>
+        /// Indexes a folder.
+        /// </summary>
+        /// <param name="directory"></param>
+        private void addFolder(ZlpDirectoryInfo directory)
+        {
+            // find all matching files
+            foreach (string pattern in patterns)
+            {
+                foreach (ZlpFileInfo fi in directory.GetFiles(pattern))
+                {
+                    // skip temporary files
+                    if (fi.Name.StartsWith("~"))
+                        continue;
+
+                    try
+                    {
+                        addDocument(fi.FullName);
+
+                        // update statistics
+                        this.countTotal++;
+                        this.bytesTotal += fi.Length;
+
+                        // show added file
+                        status(fi.FullName);
+                    }
+                    catch (Exception)
+                    {
+                        // parsing and indexing wasn't successful, skipping that file
+                        this.countSkipped++;
+                        status("Skipped: " + fi.FullName);
+                    }
+                }
+            }
+
+            // add subfolders
+            foreach (ZlpDirectoryInfo di in directory.GetDirectories())
+            {
+                addFolder(di);
+            }
+        }
+
+        /// <summary>
+        /// Parses and indexes an IFilter parseable file.
+        /// </summary>
+        /// <param name="path"></param>
+        private void addDocument(string path)
+        {
+            Document doc = new Document();
+            string filename = Path.GetFileName(path);
+
+            doc.Add(Field.UnStored("text", Parser.Parse(path)));
+            doc.Add(Field.Keyword("path", path));
+            doc.Add(Field.Text("title", filename));
+            indexWriter.AddDocument(doc);
+        }
+
+        private void buttonBrowse_Click(object sender, System.EventArgs e)
+        {
+            this.folderBrowserDialog1.SelectedPath = this.textBoxPath.Text;
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxPath.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void buttonClean_Click(object sender, System.EventArgs e)
+        {
+            Directory.Delete(this.pathIndex, true);
+            checkIndex();
+        }
+
+        private void AddToIndexButton_Click(object sender, EventArgs e)
+        {
+            BuildIndex(false);
+        }
+    }
+}
